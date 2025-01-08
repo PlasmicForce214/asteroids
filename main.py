056d4b09393eb591e5ecc5d356513bd4623bdb94
bleepbloop <bleepbloop@aol.com>
2025-01-08 04:48:39 -0500

    Asteroid spawning

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -12,19 +15,38 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock=pygame.time.Clock()
-    dt=0
+    clock = pygame.time.Clock()
+    # Delta Time
+    dt = 0
+    # Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroid_group = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroid_group)
+    AsteroidField.containers = (updatable)
+    
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    asteroid_field= AsteroidField()
+
     # Start Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        
+        # player.update(dt)
+        for obj in updatable:
+            obj.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+        
+        # player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
-        dt=clock.tick(60)/1000
+        dt = clock.tick(60)/1000
         
 
 if __name__ == "__main__":
